4fb3924a58b7603417a6acdbb1f1185a252cec56
Ivan Vrsajkov <vrsajkov.ivan@gmail.com>
2024-09-26 18:20:19 +0200

    add bold link variant and flip animation on hover

--- src/components/Header/NavLink.js
+++ src/components/Header/NavLink.js
@@ -6,22 +6,39 @@ const NavLink = ({ href, children }) => {
     return (
         <NavlinkWrapper>
             <Link href={href}>{children}</Link>
+            <BoldLink href={href}>{children}</BoldLink>
         </NavlinkWrapper>
     );
 }
 
 const Link = styled.a`
-  font-size: 1.125rem;
-  text-transform: uppercase;
-  text-decoration: none;
-  color: var(--color-gray-900);
-  font-weight: ${WEIGHTS.medium};
+    display: block;
+    font-size: 1.125rem;
+    text-transform: uppercase;
+    text-decoration: none;
+    color: var(--color-gray-900);
+    font-weight: ${WEIGHTS.medium};
+
+    transform: translateY(0);
+    transition: transform 200ms;
+`;
+
+const BoldLink = styled(Link)`
+    position: absolute;
+    font-weight: ${WEIGHTS.bold};
 `;
 
 const NavlinkWrapper = styled.div`
-  &:first-of-type > ${Link}  {
-    color: var(--color-secondary);
-  }
+    position: relative;
+    overflow: hidden;
+
+    &:first-of-type > ${Link}  {
+        color: var(--color-secondary);
+    }
+
+    &:hover > ${Link} {
+        transform: translateY(-100%);
+    }
 `;
 
 export default NavLink;
